17dd4c1940264809b6df59d4e882ceabbf80cc08
PrzemyslawKlys <pklys@evotec.pl>
2018-04-20 17:19:26 +0200

    Fixes to Write-Color, adding Examples

---
+++ Write-Color-Examples.ps1
@@ -0,0 +1,45 @@
+Clear-Host
+. "C:\Support\GitHub\PSWriteColor\Write-Color.ps1" # Include Write-Color
+
+# Example 1
+
+Write-Color "[i] ", "Parameter in configuration of ", "EmailParameters.EmailFrom", " exists." -Color White, White, Green, White -ShowTime
+Write-Color "[i] ", "Parameter in configuration of ", "EmailParameters.EmailTo", " exists." -Color White, White, Green, White -ShowTime
+
+# Example 2
+
+Write-Color "[i] ", "I will send email soon...", "Get ready.." -Color White
+Write-Color "[i] ", "Sending email...." -Color White, White -NoNewLine
+<#
+    Do Something....
+#>
+if ($true) {
+    Write-Color -Text "OK" -Color Green
+}
+
+# Example 3
+
+Write-Color -Text "Red ", "Green ", "Yellow " -Color Red, Green, Yellow
+
+Write-Color -Text "This is text in Green ",
+"followed by red ",
+"and then we have Magenta... ",
+"isn't it fun? ",
+"Here goes DarkCyan" -Color Green, Red, Magenta, White, DarkCyan
+
+Write-Color -Text "This is text in Green ",
+"followed by red ",
+"and then we have Magenta... ",
+"isn't it fun? ",
+"Here goes DarkCyan" -Color Green, Red, Magenta, White, DarkCyan -StartTab 3 -LinesBefore 1 -LinesAfter 1
+
+Write-Color "1. ", "Option 1" -Color Yellow, Green
+Write-Color "2. ", "Option 2" -Color Yellow, Green
+Write-Color "3. ", "Option 3" -Color Yellow, Green
+Write-Color "4. ", "Option 4" -Color Yellow, Green
+Write-Color "9. ", "Press 9 to exit" -Color Yellow, Gray -LinesBefore 1
+
+Write-Color -LinesBefore 2 -Text "This little ", "message is ", "written to log ", "file as well." `
+				-Color Yellow, White, Green, Red, Red -LogFile "C:\testing.txt" -TimeFormat "yyyy-MM-dd HH:mm:ss"
+Write-Color -Text "This can get ", "handy if ", "want to display things, and log actions to file ", "at the same time." `
+				-Color Yellow, White, Green, Red, Red -LogFile "C:\testing.txt"
