6773fb01a063493eb5e0abb11eac776dbe762d2e
PrzemyslawKlys <pklys@evotec.pl>
2018-10-16 12:06:49 +0200

    Added StartSpaces

--- PSWriteColor.psm1
+++ PSWriteColor.psm1
@@ -84,6 +84,7 @@ function Write-Color {
         [int] $StartTab = 0,
         [int] $LinesBefore = 0,
         [int] $LinesAfter = 0,
+        [int] $StartSpaces = 0,
         [alias ("L")] [string] $LogFile = "",
         [string] $TimeFormat = "yyyy-MM-dd HH:mm:ss",
         [alias ('LogTimeStamp')][bool] $LogTime = $true,
@@ -97,6 +98,7 @@ function Write-Color {
     if ($LinesBefore -ne 0) {  for ($i = 0; $i -lt $LinesBefore; $i++) { Write-Host -Object "`n" -NoNewline } } # Add empty line before
     if ($ShowTime) { Write-Host -Object "[$([datetime]::Now.ToString($TimeFormat))]" -NoNewline} # Add Time before output
     if ($StartTab -ne 0) {  for ($i = 0; $i -lt $StartTab; $i++) { Write-Host -Object "`t" -NoNewLine } }  # Add TABS before text
+    if ($StartSpaces -ne 0) {  for ($i = 0; $i -lt $StartSpaces; $i++) { Write-Host -Object ' ' -NoNewLine } }  # Add SPACES before text
     if ($Color.Count -ge $Text.Count) {
         # the real deal coloring
         if ($BackGroundColor -eq $null) {
