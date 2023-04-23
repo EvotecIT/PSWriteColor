0e4775af356a698b2f0d11c82561b67de2185d11
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-23 13:57:45 +0200

    Convert tabs into spaces

--- Public/Write-Color.ps1
+++ Public/Write-Color.ps1
@@ -1,59 +1,57 @@
 function Write-Color {
     <#
-	.SYNOPSIS
-        Write-Color is a wrapper around Write-Host.
+    .SYNOPSIS
+    Write-Color is a wrapper around Write-Host delivering a lot of additional features for easier color options.
 
-        It provides:
-        - Easy manipulation of colors,
-        - Logging output to file (log)
-        - Nice formatting options out of the box.
+    .DESCRIPTION
+    Write-Color is a wrapper around Write-Host delivering a lot of additional features for easier color options.
 
-	.DESCRIPTION
-        Author: przemyslaw.klys at evotec.pl
-        Project website: https://evotec.xyz/hub/scripts/write-color-ps1/
-        Project support: https://github.com/EvotecIT/PSWriteColor
+    It provides:
+    - Easy manipulation of colors,
+    - Logging output to file (log)
+    - Nice formatting options out of the box.
+    - Ability to use aliases for parameters
 
-        Original idea: Josh (https://stackoverflow.com/users/81769/josh)
-
-	.EXAMPLE
+    .EXAMPLE
     Write-Color -Text "Red ", "Green ", "Yellow " -Color Red,Green,Yellow
 
     .EXAMPLE
-	Write-Color -Text "This is text in Green ",
-					"followed by red ",
-					"and then we have Magenta... ",
-					"isn't it fun? ",
-					"Here goes DarkCyan" -Color Green,Red,Magenta,White,DarkCyan
+    Write-Color -Text "This is text in Green ",
+                      "followed by red ",
+                      "and then we have Magenta... ",
+                      "isn't it fun? ",
+                      "Here goes DarkCyan" -Color Green,Red,Magenta,White,DarkCyan
 
     .EXAMPLE
-	Write-Color -Text "This is text in Green ",
-					"followed by red ",
-					"and then we have Magenta... ",
-					"isn't it fun? ",
-                    "Here goes DarkCyan" -Color Green,Red,Magenta,White,DarkCyan -StartTab 3 -LinesBefore 1 -LinesAfter 1
+    Write-Color -Text "This is text in Green ",
+                      "followed by red ",
+                      "and then we have Magenta... ",
+                      "isn't it fun? ",
+                      "Here goes DarkCyan" -Color Green,Red,Magenta,White,DarkCyan -StartTab 3 -LinesBefore 1 -LinesAfter 1
 
     .EXAMPLE
-	Write-Color "1. ", "Option 1" -Color Yellow, Green
-	Write-Color "2. ", "Option 2" -Color Yellow, Green
-	Write-Color "3. ", "Option 3" -Color Yellow, Green
-	Write-Color "4. ", "Option 4" -Color Yellow, Green
-	Write-Color "9. ", "Press 9 to exit" -Color Yellow, Gray -LinesBefore 1
+    Write-Color "1. ", "Option 1" -Color Yellow, Green
+    Write-Color "2. ", "Option 2" -Color Yellow, Green
+    Write-Color "3. ", "Option 3" -Color Yellow, Green
+    Write-Color "4. ", "Option 4" -Color Yellow, Green
+    Write-Color "9. ", "Press 9 to exit" -Color Yellow, Gray -LinesBefore 1
 
     .EXAMPLE
-	Write-Color -LinesBefore 2 -Text "This little ","message is ", "written to log ", "file as well." `
-				-Color Yellow, White, Green, Red, Red -LogFile "C:\testing.txt" -TimeFormat "yyyy-MM-dd HH:mm:ss"
-	Write-Color -Text "This can get ","handy if ", "want to display things, and log actions to file ", "at the same time." `
-				-Color Yellow, White, Green, Red, Red -LogFile "C:\testing.txt"
+    Write-Color -LinesBefore 2 -Text "This little ","message is ", "written to log ", "file as well." `
+                -Color Yellow, White, Green, Red, Red -LogFile "C:\testing.txt" -TimeFormat "yyyy-MM-dd HH:mm:ss"
+    Write-Color -Text "This can get ","handy if ", "want to display things, and log actions to file ", "at the same time." `
+                -Color Yellow, White, Green, Red, Red -LogFile "C:\testing.txt"
 
     .EXAMPLE
-    # Added in 0.5
     Write-Color -T "My text", " is ", "all colorful" -C Yellow, Red, Green -B Green, Green, Yellow
-    wc -t "my text" -c yellow -b green
-    wc -text "my text" -c red
+    Write-Color -t "my text" -c yellow -b green
+    Write-Color -text "my text" -c red
 
     .NOTES
-        Additional Notes:
-        - TimeFormat https://msdn.microsoft.com/en-us/library/8kb3ddd4.aspx
+    Understanding Custom date and time format strings: https://learn.microsoft.com/en-us/dotnet/standard/base-types/custom-date-and-time-format-strings
+    Project support: https://github.com/EvotecIT/PSWriteColor
+    Original idea: Josh (https://stackoverflow.com/users/81769/josh)
+
     #>
     [alias('Write-Colour')]
     [CmdletBinding()]
@@ -78,7 +76,6 @@ function Write-Color {
         Write-Error "Colors, BackGroundColors parameters count doesn't match. Terminated."
         return
     }
-    #if ($Text.Count -eq 0) { return }
     if ($LinesBefore -ne 0) { for ($i = 0; $i -lt $LinesBefore; $i++) { Write-Host -Object "`n" -NoNewline } } # Add empty line before
     if ($StartTab -ne 0) { for ($i = 0; $i -lt $StartTab; $i++) { Write-Host -Object "`t" -NoNewline } }  # Add TABS before text
     if ($StartSpaces -ne 0) { for ($i = 0; $i -lt $StartSpaces; $i++) { Write-Host -Object ' ' -NoNewline } }  # Add SPACES before text
@@ -122,7 +119,7 @@ function Write-Color {
                 $Saved = $true
             } catch {
                 if ($Saved -eq $false -and $Retry -eq $LogRetry) {
-                    $PSCmdlet.WriteError($_)
+                    Write-Warning "Write-Color - Couldn't write to log file $($_.Exception.Message). Tried ($Retry/$LogRetry))"
                 } else {
                     Write-Warning "Write-Color - Couldn't write to log file $($_.Exception.Message). Retrying... ($Retry/$LogRetry)"
                 }
